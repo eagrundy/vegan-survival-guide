2ab093aa062908c9a42c1b384b183760c9abc98a
eagrundy <ericamalakian@gmail.com>
2020-12-12 17:44:28 -0800

    CREATE functionality for restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,15 +1,34 @@
 class RestaurantsController < ApplicationController
+    # CRUD
 
+    # READ
+    # index, route for all restaurants posts
     get '/restaurants' do
         @restaurants = Restaurant.all
-
+        # render all restaurants posts
         erb :'restaurants/index'
     end
 
-    get '/restaurants/:id' do
-        @restaurant = Restaurant.find(params[:id])
-        erb :'/restaurants/show'
+    
+    # CREATE
+    # render a form to create a new post
+    get "/restaurants/new" do
+        erb :"restaurants/new"
     end
 
+    post "/restaurants" do
+        restaurant = Restaurant.create(name: params[:name], options: params[:options], source: params[:source], image_url: params[:image_url], user_id: current_user.id)
+        redirect "/restaurants/#{post.id}"
+    end
+    
+    
+    # show route for a single restaurant post
+    get '/restaurants/:id' do
+        # find the post
+        @restaurant = Restaurant.find(params[:id])
+        erb :"/restaurants/show"
+    end
+
+    
 
 end
